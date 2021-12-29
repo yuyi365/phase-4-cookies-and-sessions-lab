1c354446ed81fe76ece9af96d9fe3a2d7a164c13
Yuyi <yuyi365@gmail.com>
2021-12-29 10:08:18 -0500

    commit tree a06182f0449ba146a177f465111a8156dfc36b60

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,14 +7,23 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+
+    if session[:page_views] <= 3
+      article = Article.find(params[:id])
+      render json: article
+    else
+      render json: {
+               error: 'Maximum pageview limit reached',
+             },
+             status: :unauthorized
+    end
   end
 
   private
 
   def record_not_found
-    render json: { error: "Article not found" }, status: :not_found
+    render json: { error: 'Article not found' }, status: :not_found
   end
-
 end
--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,16 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
+require 'active_model/railtie'
 # require "active_job/railtie"
-require "active_record/railtie"
+require 'active_record/railtie'
 # require "active_storage/engine"
-require "action_controller/railtie"
+require 'action_controller/railtie'
 # require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
-require "action_view/railtie"
+require 'action_view/railtie'
 # require "action_cable/engine"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
@@ -35,7 +35,7 @@ module Phase4CookiesAndSessionsLab
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true    
+    config.api_only = true
 
     # Adding back cookies and session middleware
     config.middleware.use ActionDispatch::Cookies
@@ -44,6 +44,5 @@ module Phase4CookiesAndSessionsLab
     # Use SameSite=Strict for all cookies to help protect against CSRF
     # https://owasp.org/www-community/SameSite
     config.action_dispatch.cookies_same_site_protection = :strict
-
   end
 end
